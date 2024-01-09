e53aa934d187564f3ee4bdeafe1b95fdd7f2666e
Anubhav Singh <Anubhav.singh@worka.me>
2024-01-09 11:07:19 +0530

    Recent context changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { TodoProvider, TodoContext } from "./context";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todo, setTodo] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodo((prev) => [{ id: date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodo((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodo((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggelCompleter = (id) => {
+    setTodo((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed  }
+          : prevTodo
+      )
+    );
+  };
 
   return (
-    <>
-     <h1 className='underline text-3xl'>Hello world</h1>
-    </>
-  )
+    <TodoProvider
+      value={{ todos, addTodo, removeTodo, updateTodo, toggelCompleter }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
